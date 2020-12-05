c9a7ef071614f80dce76fcb8700f70e94513b3a5
Alexis <allexis.bal@hotmail.fr>
2020-12-05 17:15:57 -0500

    Game pallier 1

---
+++ Assets/Alexis/Script/LightScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    private void Start()
+    {
+        RenderSettings.ambientLight = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+}
