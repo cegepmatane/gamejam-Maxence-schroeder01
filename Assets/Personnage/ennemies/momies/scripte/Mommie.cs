e28a47f0244a4b581f944af7830c7a32ed24787a
Maxence Schroeder <maxence.schroeder@gmail.com>
2020-12-05 23:16:35 -0500

    momie pathfinding

---
+++ Assets/Personnage/ennemies/momies/scripte/Mommie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Mommie : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        anim.SetBool("move", true);
+        anim.SetFloat("velx", (player.position.x - transform.position.x));
+        anim.SetFloat("vely", (player.position.y - transform.position.y));
+    }
+}
